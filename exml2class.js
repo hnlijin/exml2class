3693fb2d48f1b65cc66baf764d353a09cfe4a849
李进 <lijin@lijindeMac-mini.local>
2017-03-27 00:00:58 +0800

    parse xml

--- exml2class.js
+++ exml2class.js
@@ -1,39 +1,74 @@
 var fs = require("fs");
 var path = require("path");
-var ejs = require("ejs")
+var ejs = require("ejs");
+var DOMParser = require('xmldom').DOMParser;
 
-var root = path.join(__dirname, "..", "resource", "ui");
-var temp = fs.readFileSync(path.join(__dirname, "..", "exml2class", "class.ejs"), "utf-8");
-var fileList = []
-readDirSync(root, fileList)
-console.log(temp)
+function EXml2Class()
+{
+}
 
-var data = {}
-data.class = {}
-data.class.name = "MainUI"
-// var template = ejs.compile(temp)
-// template(data)
-var s = ejs.render(temp, data)
-console.log(s)
+module.export = EXml2Class;
 
-function readDirSync(dir, fileList)
+EXml2Class.prototype.constructor = function()
+{
+
+}
+
+EXml2Class.prototype.parse = function()
+{
+	var root = path.join(__dirname, "..", "resource", "ui");
+	var classTemplete = fs.readFileSync(path.join(__dirname, "..", "exml2class", "class.ejs"), "utf8");
+	var fileList = [];
+	this.readDirSync(root, fileList);
+	fileList.forEach(function(fn, index){
+		var data = this.createEClassOptions();
+		var xmlStr = fs.readFileSync(fn, "utf8");
+		var xml = new DOMParser().parseFromString(xmlStr, 'text/xml');
+		this.parseEXml(xml, data);
+	}.bind(this));
+}
+
+// var data = createEClassOptions();
+// ejs.compile(classTemplete);
+// var s = ejs.render(classTemplete, data);
+// console.log(s);
+
+EXml2Class.prototype.parseEXml = function(node, eclassData)
+{
+	console.log("kkk:", node.nodeName);
+	if (node.hasChildNodes())
+	{
+		for(var item in node.childNodes)
+		{
+			// this.parseEXml(item);
+		}
+	}
+}
+
+EXml2Class.prototype.readDirSync = function(dir, fileList)
 {
 	var pa = fs.readdirSync(dir);
 	pa.forEach(function(fn, index) {
-		console.log(fn, index)
-		var fp = path.join(dir, fn)
-		var finfo = fs.statSync(fp)
+		var fp = path.join(dir, fn);
+		var finfo = fs.statSync(fp);
 		if (finfo.isDirectory()) {
-			readDirSync(fp, fileList)
+			readDirSync(fp, fileList);
 		} else {
-			var ext = path.extname(fp)
+			var ext = path.extname(fp);
 			if (ext == ".exml") {
-				fileList.push(fileList, fp)
+				fileList.push(fp);
 			}
 		}
 	});
 }
 
-fileList.forEach(function(fn, index){
-	console.log(fn, index)
-});
+EXml2Class.prototype.createEClassOptions = function()
+{
+	var data = {}
+	data.eclass = {}; 
+	data.eclass.enamespace = "eui."
+	data.eclass.ename = "";
+	data.eclass.eskin = "";
+	data.eclass.eids = []; // {eid: "", eclass: ""}
+	return data;
+}
